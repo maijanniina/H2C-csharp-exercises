d9f55806292f3c526c0d2c169311de08d5f6114a
maijanniina <maija.virta@centria.fi>
2020-11-01 19:41:30 +0200

    1.11.2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -6,8 +6,14 @@ namespace exercise_10
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
-
+            Console.Write("I will tell a story, but I need some information.\n" + "Give a name for main character:");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.Write("Give the character a profession:");
+            string answer = Console.ReadLine();
+            Console.WriteLine("Here is the story:\n" + "Once upon a time there was a " + answer + "called " + name);
+            Console.WriteLine("On her way to work, " + name + "often pondered what being " + answer + "meant to them.");
+            Console.WriteLine("When you work as a " + answer + " you meet interesting people.\n" + name + " enjoys their work as " + answer + ", The end.");
         }
     }
 }
