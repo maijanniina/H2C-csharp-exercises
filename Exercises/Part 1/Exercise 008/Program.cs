ac3841a5bb2890c31397325e04af2895ab98a672
maijanniina <maija.virta@centria.fi>
2020-11-01 18:56:56 +0200

    1.11.2020

--- Exercises/Part 1/Exercise 008/Program.cs
+++ Exercises/Part 1/Exercise 008/Program.cs
@@ -6,7 +6,9 @@ namespace exercise_08
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.Write("What is your name?");
+            string message = Console.ReadLine();
+            Console.WriteLine("Hello " + message + "!");
 
         }
     }
