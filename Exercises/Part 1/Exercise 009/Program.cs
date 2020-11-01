d43ecd5017f5349f74168ae4a9015e09528d61fb
maijanniina <maija.virta@centria.fi>
2020-11-01 19:12:54 +0200

    1.11.2020

--- Exercises/Part 1/Exercise 009/Program.cs
+++ Exercises/Part 1/Exercise 009/Program.cs
@@ -6,7 +6,11 @@ namespace exercise_09
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.Write("Hello, how are you?");
+            string message = Console.ReadLine();
+            Console.Write("That is interesting, tell me more");
+            string answer = Console.ReadLine();
+            Console.WriteLine("Thank you for sharing!");
 
         }
     }
