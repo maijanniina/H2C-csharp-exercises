a56980315858e236cc61aa7b742c0bc4831e1d7b
maijanniina <maija.virta@centria.fi>
2020-11-02 11:51:36 +0200

    2.11.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,25 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.Write("Give a greeting: ");
+            string answer = Console.ReadLine();
+            Console.WriteLine(answer);
+            Console.Write("Give one number: ");
+            string userInput = Console.ReadLine();
+            int intValue = Convert.ToInt32(userInput);
+            Console.WriteLine(intValue);
+            Console.Write("Give a double: ");
+            userInput = Console.ReadLine();
+            double doubleValue = Convert.ToDouble(userInput);
+            Console.WriteLine(doubleValue);
+            Console.Write("Give a boolean: ");
+            string truth = "True"; 
+            bool booleanValue = System.Convert.ToBoolean(truth);
+            Console.WriteLine(booleanValue);
+            Console.WriteLine("Your greeting: " + answer);
+            Console.WriteLine("Your number is: " + intValue);
+            Console.WriteLine("Your double is: " + doubleValue);
+            Console.WriteLine("Your boolean is: " + booleanValue);
 
     }
   }
