dabb6def9924cdf096fc729e86a900fb63ab99bb
maijanniina <maija.virta@centria.fi>
2020-11-02 11:18:00 +0200

    2.11.2020

--- Exercises/Part 1/Exercise 013/Program.cs
+++ Exercises/Part 1/Exercise 013/Program.cs
@@ -6,7 +6,10 @@ namespace exercise_13
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.Write("Give a number! ");
+          userInput = Console.ReadLine();
+            double doubleValue = Convert.toDouble(userInput);
+            Console.WriteLine("You gave " + doubleValue);
 
     }
   }
