6649af4736fa24f6a8705451d9e76b7efa03d10b
maijanniina <maija.virta@centria.fi>
2020-11-02 14:02:15 +0200

    2.11.2020

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.Write("Give the first number! ");
+            string userInput = Console.ReadLine();
+            int intValue = Convert.ToInt32(userInput);
+            Console.Write("Give the second number! ");
+            string userInput2 = Console.ReadLine();
+            int intValue2 = Convert.ToInt32(userInput2);
+            Console.Write("Give the third number! ");
+            string userInput3 = Console.ReadLine();
+            int intValue3 = Convert.ToInt32(userInput3);
+            Console.WriteLine("The sum is " + (intValue + intValue2 + intValue3));
 
     }
   }
