4303617de744458362ba35c282c45b4a57e12965
maijanniina <maija.virta@centria.fi>
2020-11-04 22:46:05 +0200

    4.11.2020

--- Exercises/Part 1/Exercise 024/Program.cs
+++ Exercises/Part 1/Exercise 024/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_24
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.Write("Confirm your speed:");
+            string userInput = Console.ReadLine();
+            int intValue = Convert.ToInt32(userInput);
+            Console.Write("Your speed is " + intValue + "\n" );
+            if (intValue > 120)
+            {
+                Console.Write("You are speeding! \n");
+            } 
+            else
+            {
+                Console.Write("You are not speeding, good! \n");
+            } 
       
     }
   }
