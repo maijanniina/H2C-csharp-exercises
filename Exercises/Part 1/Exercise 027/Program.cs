603e7dc364bcc67f344317c364ffc947caeaa6bd
maijanniina <maija.virta@centria.fi>
2020-11-07 18:35:14 +0200

    7.11.2020

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.Write("Give a number: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+            int intValue = Convert.ToInt32(userInput);
+            if (intValue >= 0)
+            {
+                Console.WriteLine("It is positive ");
+            }
+            else if (intValue < -1)
+            {
+                Console.WriteLine("It is not positive ");
+            }
 
     }
   }
