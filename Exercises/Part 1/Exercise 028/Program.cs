8c073813540c254053088e9db1294113751c75fe
maijanniina <maija.virta@centria.fi>
2020-11-07 18:40:55 +0200

    7.11.2020

--- Exercises/Part 1/Exercise 028/Program.cs
+++ Exercises/Part 1/Exercise 028/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_28
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.Write("How old are you? ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+            int intValue = Convert.ToInt32(userInput);
+            if (intValue < 18)
+            {
+                Console.WriteLine("You're under age! ");
+            }
+            else if (intValue >= 18)
+            {
+                Console.WriteLine("You're an adult! ");
+            }
 
     }
   }
