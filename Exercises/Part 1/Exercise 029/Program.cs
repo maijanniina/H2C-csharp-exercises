b6aeeee0dfd7d26658b056c42dca4418d884a300
maijanniina <maija.virta@centria.fi>
2020-11-07 19:01:29 +0200

    7.11.2020

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,7 +6,27 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.Write("Give the first number! ");
+            string userInput = Console.ReadLine(); Console.WriteLine(userInput);
+            int intValue = Convert.ToInt32(userInput);
+            Console.Write("Give the second number! ");
+            string userInput2 = Console.ReadLine(); Console.WriteLine(userInput2);
+            int intValue2 = Convert.ToInt32(userInput2);
+            bool isFirstLargerThanSecond = intValue > intValue2;
+            bool isFirstSmallerThanSecond = intValue < intValue2;
+            bool truthValue = intValue == intValue2;
+            if (isFirstLargerThanSecond)
+            {
+                Console.Write("The larger number is "+ intValue);
+            }
+            else if (isFirstSmallerThanSecond)
+            {
+                Console.WriteLine("The larger number is " + intValue2);
+            }
+            else if (truthValue)
+            {
+                Console.WriteLine("They are equal! ");
+            }
 
     }
   }
