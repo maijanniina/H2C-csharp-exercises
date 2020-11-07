4aa1b3b0048eb33e20fa5cd2eb4bc467805819fe
maijanniina <maija.virta@centria.fi>
2020-11-07 18:26:36 +0200

    7.11.2020

--- Exercises/Part 1/Exercise 026/Program.cs
+++ Exercises/Part 1/Exercise 026/Program.cs
@@ -6,8 +6,18 @@ namespace exercise_26
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      Console.Write("What is your birth year?: ");
+            string userInput = Console.ReadLine(); 
+            Console.WriteLine(userInput);
+            int intValue = Convert.ToInt32(userInput);
+            if ( intValue <= 1900)
+            {
+                Console.Write("You're old!");
+            }
+              else if (intValue > 1901)
+            {
+                Console.Write("You're in the best age of your life!");
+            }
     }
   }
 }
