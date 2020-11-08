2906f654dfcd00f7b79b14d6766a8809bfb00a1e
maijanniina <maija.virta@centria.fi>
2020-11-08 21:57:02 +0200

    8.11.2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,8 +6,41 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+      Console.Write("Give your percent [0 - 100]: ");
+            string userInput = Console.ReadLine(); Console.WriteLine(userInput);
+            int intValue = Convert.ToInt32(userInput);
+            if (intValue < 0 )
+            {
+                Console.Write("Impossible ");
+            }
+            else if ( intValue >= 0 && intValue <= 49 )
+            {
+                Console.Write("Fail ");
+            }
+            else if (intValue >= 50 && intValue <= 59)
+            {
+                Console.Write("Grade 1 ");
+            }
+            else if (intValue >= 60 && intValue <= 69)
+            {
+                Console.Write("Grade 2 ");
+            }
+            else if ( intValue >= 70 && intValue <= 79)
+            {
+                Console.Write("Grade 3 ");
+            }
+            else if ( intValue >= 80 && intValue <= 89)
+            {
+                Console.Write("Grade 4 ");
+            }
+            else if ( intValue >= 90 && intValue <= 100)
+            {
+                Console.Write("Grade 5 ");
+            }
+            else if ( intValue > 100)
+            {
+                Console.Write("Outstanding! ");
+            }
     }
   }
 }
