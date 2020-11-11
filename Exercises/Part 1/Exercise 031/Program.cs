32c46b2e46f03533aa81204be9061acc301c1230
maijanniina <maija.virta@centria.fi>
2020-11-11 19:33:40 +0200

    11.11.2020

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,7 +6,10 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.Write("Give a number: ");
+            string userInput = Console.ReadLine();
+            int intValue = Convert.ToInt32(userInput);
+            Console.Write(intValue % 2 == 0 ? "Your integer ({0}) is even." : "Your integer ({0}) is odd.", intValue);
 
     }
   }
