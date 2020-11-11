0e23a9a4db74aba83fff3aacb75d382ba89bcfa4
maijanniina <maija.virta@centria.fi>
2020-11-11 20:11:28 +0200

    11.11.2020

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,7 +6,22 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.Write("Give the first string: ");
+            string userWord = Console.ReadLine();
+            Console.WriteLine(userWord);
+            string a = userWord;
+       Console.Write("Give the second string: ");
+            string usermessage = Console.ReadLine();
+            string b = usermessage;
+            Console.WriteLine(usermessage);
+     if ( a == b )
+     {
+                Console.Write("Echo! ");
+     }
+     else
+     {
+                Console.Write("Nope! ");
+     }
 
     }
   }
