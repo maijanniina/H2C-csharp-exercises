0628ac98c1e7b15c8e142d0977132ef1751325df
maijanniina <maija.virta@centria.fi>
2020-11-14 20:26:07 +0200

    14.11.2020

--- Exercises/Part 1/Exercise 039/Program.cs
+++ Exercises/Part 1/Exercise 039/Program.cs
@@ -6,7 +6,31 @@ namespace exercise_39
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+       int sum1 = 0;
+       int sum2 = 0;
+       
+       while (true)
+       {
+            Console.Write("Give a number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number == 0)
+            {
+                    break;
+            }
+
+            if ( number < 0)
+            {
+                    sum1 = sum1 + number;
+            }
+
+            if ( number > 0)
+            {
+                    sum2 = sum2 + number;
+            }
+       }
+
+        Console.WriteLine("Total sum of numbers: " + (sum1 + sum2));
     }
+
   }
 }
