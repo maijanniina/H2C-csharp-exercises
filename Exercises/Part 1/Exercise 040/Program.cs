5e2c36d1708fa8232ebb1d8a9dbecf6ff5f1cb74
maijanniina <maija.virta@centria.fi>
2020-11-14 20:39:31 +0200

    14.11.2020

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -6,8 +6,39 @@ namespace exercise_40
   {
     public static void Main(string[] args)
     {
+        int sum1 = 0;
+        int sum2 = 0;
+        int amount = 0;
+        int amount1 = 0;
 
-      // Write your code here:
+        while(true)
+        {
+                Console.WriteLine("Give a number" );
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                {
+                    break;
+                }
+
+                if (number < 0)
+                {
+
+                    sum1 = sum1 + number; 
+                    amount = amount + 1;
+
+                }
+
+                if (number > 0)
+                {
+
+                    sum2 = sum2 + number; 
+                    amount1 = amount1 + 1;
+
+                }
+        }
+
+        Console.WriteLine("Total amount of numbers: " + (amount + amount1)); Console.Write("& the sum of the numbers: " + (sum1 + sum2));
+      
     }
   }
 }
