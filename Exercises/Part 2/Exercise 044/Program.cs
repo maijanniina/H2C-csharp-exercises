5ee9ebe378667a01d6dd2fcd990b363140e13cbf
maijanniina <maija.virta@centria.fi>
2020-11-15 12:42:54 +0200

    15.11.2020

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -7,8 +7,23 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      int first = Convert.ToInt32(Console.ReadLine());
+      int second = Convert.ToInt32(Console.ReadLine());
+      if (first > second)
+      {
+                Console.Write( first + " is grEater than " + second);
+      }
 
+      if (first < second)
+      {
+                Console.Write(first + " is less than " + second);
+      }
+
+      if (first == second)
+      {
+                Console.Write(first + " is equal to " + second);
+
+      }
     }
   }
 }
