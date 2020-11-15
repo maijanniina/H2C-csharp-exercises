257a756796db806eea292adc2c12f6b46343fac7
maijanniina <maija.virta@centria.fi>
2020-11-15 12:08:04 +0200

    15.11.2020

--- Exercises/Part 2/Exercise 041/Program.cs
+++ Exercises/Part 2/Exercise 041/Program.cs
@@ -7,7 +7,6 @@ namespace exercise_41
     public static void Main(string[] args)
     {
 
-      Console.WriteLine("Give a number ");
       int userInput = Convert.ToInt32(Console.ReadLine());
       
       int sum = userInput * userInput;
