450a10e53b4b68053dce0e466737a3f0b50a52e1
maijanniina <maija.virta@centria.fi>
2020-11-23 20:40:24 +0200

    23.11.2020

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -7,7 +7,7 @@ namespace exercise_47
     public static void Main(string[] args)
     {
         Console.Write("Where to?");
-        int end = Convert.ToInt32(Console.ReadLine());
+        int end = Convert.ToInt32(Console.ReadLine()); 
         Console.Write("Where from? ");
         int start = Convert.ToInt32(Console.ReadLine());
         for (int i = start; i <= end; i++)
--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,9 +6,35 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+       Console.WriteLine("Give numbers: ");
+       int sum = 0;
+       int givenNumbers = 0;
 
-      // Write your code here:
+           while(true)
+           {
+           
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                if ( userInput == -1)
+
+                {
+                   Console.WriteLine("Thx! Bye! ");
+                   Console.WriteLine(sum);
+                   break;
+                }
+
+                if (userInput != -1) 
+
+                {
+                    sum += userInput;
+                    givenNumbers++;
+                    continue;
+
+                }            
  
+
+           }
+          
     }
   }
 }
