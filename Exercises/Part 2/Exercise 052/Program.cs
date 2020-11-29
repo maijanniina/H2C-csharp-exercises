bc4b279ee38992c8244f8ceafbff711ba0e3f7f4
maijanniina <maija.virta@centria.fi>
2020-11-29 14:44:09 +0200

    29.11.2020

--- Exercises/Part 2/Exercise 052/Program.cs
+++ Exercises/Part 2/Exercise 052/Program.cs
@@ -7,10 +7,22 @@ namespace exercise_52
     public static void Main(String[] args)
     {
       // Call your method here:
+      int numberStart = Convert.ToInt32(Console.ReadLine());
+      int numberEnd = Convert.ToInt32(Console.ReadLine());
+      PrintFromNumberToOne(numberStart, numberEnd);
 
     }
 
-    // Write your method here:
+         // Write your method here:
+          public static void PrintFromNumberToOne(int numberStart,int numberEnd)
+          {
+                while ( numberStart <= numberEnd)
+                {
+                    Console.WriteLine(numberStart);
+                    numberStart++;
+                }
+        
+          }
 
   }
 }
