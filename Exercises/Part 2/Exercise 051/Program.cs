588eb7c1645cd26e02ac0787db7e329b761025d3
maijanniina <maija.virta@centria.fi>
2020-11-29 14:23:41 +0200

    29.11.2020

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -4,14 +4,24 @@ namespace exercise_51
 {
   class Program
   {
-    public static void Main(String[] args)
+    public static void Main(string[] args)
     {
       // Call your method here:
-
+      int number = Convert.ToInt32(Console.ReadLine());
+      PrintUntilNumber(number);
 
     }
 
-    // Write your method here:
+        // Write your method here:
+        public static void PrintUntilNumber(int PrintUntilNumber) 
+        {
+            int i = 0;
+            while (i <= PrintUntilNumber)
+            {
+                Console.WriteLine(i);
+                i++;
 
+            }
+        }
   }
 }
