1df5a82bbefdb82e8f9868001f14dee3609925bb
maijanniina <maija.virta@centria.fi>
2020-11-29 13:50:35 +0200

    29.11.2020

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -7,12 +7,24 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+    Console.WriteLine("How many times? ");
+    int UserInput = Convert.ToInt32(Console.ReadLine());
+     PrintPhrase(UserInput);
+    
     }
 
-    // Write your method here:
-    public static void PrintPhrase() {
+          // Write your method here:
+           public static void PrintPhrase(int numOfTimes)       
+           {    
+                int i = 0;
+                while ( i < numOfTimes)
+                {
+                    Console.WriteLine("In a hole in the ground there lived a method"); 
+                    i++;
+                }
+           }
 
-    }
+           
+    
   }
 }
