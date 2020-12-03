0974e821311186dee936f00473acb47da5244fff
maijanniina <maija.virta@centria.fi>
2020-12-03 21:30:19 +0200

    3.12.2020

--- Exercises/Part 2/Exercise 054/Program.cs
+++ Exercises/Part 2/Exercise 054/Program.cs
@@ -7,10 +7,24 @@ namespace exercise_54
     public static void Main(String[] args)
     {
       // Call your method here:
+      int beginning = Convert.ToInt32(Console.ReadLine());
+      int end = Convert.ToInt32(Console.ReadLine());
+      DivisibleByThreeInRange(beginning, end);
 
     }
 
-    // Write your method here:
+        // Write your method here:
+        public static void DivisibleByThreeInRange(int beginning, int end)
+        {
+            for (int i = beginning; i <= end; i++)
+            {
+            
+              if (i % 3 == 0)
+              {
+                    Console.WriteLine(i);
+              }
+            }
+        }
 
   }
 }
